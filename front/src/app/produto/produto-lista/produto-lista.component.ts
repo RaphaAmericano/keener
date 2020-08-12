e00e52eda1f0df1910be83d3993c85d0ee8bcdc2
Raphael Americano <raphaelamericano@MacBook-Pro-de-Claudia.local>
2020-08-12 16:06:32 -0300

    Ajustes na listagem de produtos:

--- front/src/app/produto/produto-lista/produto-lista.component.ts
+++ front/src/app/produto/produto-lista/produto-lista.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { ProdutoService } from 'src/app/core/services/produto.service';
 import { Produto } from '../../shared/models/produto';
+import { MatSort } from '@angular/material/sort';
 @Component({
   selector: 'app-produto-lista',
   templateUrl: './produto-lista.component.html',
@@ -12,13 +13,17 @@ export class ProdutoListaComponent implements OnInit {
   produtos: Produto[];
   source = new MatTableDataSource(this.produtos);
   colunas: string[] = ['nome', 'quantidade'];
+  @ViewChild(MatSort, {static: true } ) sort: MatSort;
   constructor(private produtoService:ProdutoService) { }
 
   ngOnInit(): void {
     this.produtoService.buscarTodosProdutos().subscribe(
       res => this.produtos = res,
       error => console.error(error),
-      () => this.source.data = this.produtos
+      () => {
+        this.source.data = this.produtos;
+        this.source.sort = this.sort;    
+      }
     )
   }
 
